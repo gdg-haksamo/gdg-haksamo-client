7686ca0b6ef0fa348cd3ed637825c57e932aa56d
wnhaoo <153691178+wnhaoo@users.noreply.github.com>
2026-05-27 14:34:48 +0900

    feat: SelectRestaurant 구현

---
+++ src/components/mypage/SelectRestaurant.tsx
@@ -0,0 +1,26 @@
+import { PencilLine } from 'lucide-react'
+
+export function SelectRestaurant() {
+  return (
+    <div className="bg-[#FFFFF] rounded-[12px] p-5 border border-[#E0E0E0]">
+      <div className="flex flex-col gap-3">
+        <div className="flex items-center justify-between">
+          <div className="text-[14px] font-bold">자주 가는 식당</div>
+          <div className="cursor-pointer flex items-center gap-1">
+            <PencilLine size={12} className="text-[#E31E2D]" />
+            <div className="text-[12px] font-regular text-[#E31E2D]">수정</div>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <div className="flex items-center justify-center rounded-[20px] bg-[#F0F0F0] px-4 py-2 text-[12px] font-semibold text-[#606060]">
+            공식당
+          </div>
+          <div className="flex items-center justify-center rounded-[20px] bg-[#F0F0F0] px-4 py-2 text-[12px] font-semibold text-[#606060]">
+            정보센터
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,10 +1,12 @@
 import { LogOut } from 'lucide-react'
 import { ProfileCard } from '@/components/mypage/ProfileCard'
+import { SelectRestaurant } from '@/components/mypage/SelectRestaurant'
 
 export default function MyPage() {
   return (
     <div className="flex flex-1 flex-col gap-5 p-5">
       <ProfileCard />
+      <SelectRestaurant />
 
       <button className="cursor-pointer flex w-full items-center justify-center gap-2 rounded-[12px] border border-[#E0E0E0] bg-white py-4 text-[14px] font-bold text-[#A0A0A0]">
         <LogOut size={18} className="text-[#A0A0A0]" />
