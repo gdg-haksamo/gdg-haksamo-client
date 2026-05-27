7e65981322b797f546d11f656d133882b75af88f
wnhaoo <153691178+wnhaoo@users.noreply.github.com>
2026-05-27 14:49:05 +0900

    feat: SelectKeyword 구현

--- src/components/mypage/SelectKeyword.tsx
+++ src/components/mypage/SelectKeyword.tsx
@@ -1,26 +1,26 @@
 import { PencilLine } from 'lucide-react'
 
-export function SelectRestaurant() {
-  const restaurants = ['공식당', '정보센터']
+export function SelectKeyword() {
+  const keywords = ['매운음식', '면류', '육류', '해산물', '채식', '일식', '양식', '고단백']
 
   return (
-    <div className="bg-[#FFFFF] rounded-[12px] p-5 border border-[#E0E0E0]">
+    <div className="bg-white rounded-[12px] p-5 border border-[#E0E0E0]">
       <div className="flex flex-col gap-3">
         <div className="flex items-center justify-between">
-          <div className="text-[14px] font-bold">자주 가는 식당</div>
+          <div className="text-[14px] font-bold">선호 음식 키워드</div>
           <div className="cursor-pointer flex items-center gap-1">
             <PencilLine size={12} className="text-[#E31E2D]" />
             <div className="text-[12px] font-regular text-[#E31E2D]">수정</div>
           </div>
         </div>
 
-        <div className="flex items-center gap-2">
-          {restaurants.map((restaurant) => (
+        <div className="flex flex-wrap items-center gap-2">
+          {keywords.map((keyword) => (
             <div
-              key={restaurant}
+              key={keyword}
               className="flex items-center justify-center rounded-[20px] bg-[#F0F0F0] px-4 py-2 text-[12px] font-semibold text-[#606060]"
             >
-              {restaurant}
+              {keyword}
             </div>
           ))}
         </div>
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,12 +1,14 @@
 import { LogOut } from 'lucide-react'
 import { ProfileCard } from '@/components/mypage/ProfileCard'
 import { SelectRestaurant } from '@/components/mypage/SelectRestaurant'
+import { SelectKeyword } from '@/components/mypage/SelectKeyword'
 
 export default function MyPage() {
   return (
     <div className="flex flex-1 flex-col gap-5 p-5">
       <ProfileCard />
       <SelectRestaurant />
+      <SelectKeyword />
 
       <button className="cursor-pointer flex w-full items-center justify-center gap-2 rounded-[12px] border border-[#E0E0E0] bg-white py-4 text-[14px] font-bold text-[#A0A0A0]">
         <LogOut size={18} className="text-[#A0A0A0]" />
