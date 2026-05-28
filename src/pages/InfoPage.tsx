8ec08e7f7c7a67ff8b2a98cb3982282132533f31
wnhaoo <153691178+wnhaoo@users.noreply.github.com>
2026-05-28 13:57:46 +0900

    feat: RestaurantInfo 구현

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,10 +1,12 @@
 import MealInfo from '@/components/info/MealInfo'
-import { MOCK_MEAL_INFO } from '@/mocks/info'
+import RestaurantInfo from '@/components/info/RestaurantInfo'
+import { MOCK_MEAL_INFO, MOCK_RESTAURANT_INFO } from '@/mocks/info'
 
 export default function InfoPage() {
   return (
     <div className="flex flex-col gap-5 p-5">
       <MealInfo {...MOCK_MEAL_INFO} />
+      <RestaurantInfo {...MOCK_RESTAURANT_INFO} />
     </div>
   )
 }
