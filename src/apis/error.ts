6a2d584db9e430ce2f3922eb2e8a0c0625287744
seongmin <sm4046@naver.com>
2026-06-24 20:28:52 +0900

    [#21]fix: erasableSyntaxOnly 호환을 위해 ApiError 클래스 readonly 필드 제거

--- src/apis/error.ts
+++ src/apis/error.ts
@@ -1,10 +1,11 @@
 export class ApiError extends Error {
-  constructor(
-    public readonly code: string,
-    message: string,
-    public readonly status?: number,
-  ) {
+  code: string
+  status: number | undefined
+
+  constructor(code: string, message: string, status?: number) {
     super(message)
     this.name = 'ApiError'
+    this.code = code
+    this.status = status
   }
 }
